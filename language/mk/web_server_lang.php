c4a42585bf91f01d1f9ddd3303d97b626acf7f94
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:35:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'Алијаси';
+$lang['web_server_aliases_invalid'] = 'поле адреси не е валиден.';
+$lang['web_server_app_description'] = 'апликација на веб серверот може да се користи за да се создаде едноставен веб-сајтови самостојни или како дел од една поширока инфраструктура за распоредување на веб-базирани апликации со употреба на технологии како PHP, MySQL, и JavaScript.';
+$lang['web_server_app_name'] = 'веб Сервер';
+$lang['web_server_configure_default_web_site'] = 'Конфигурирај Стандардна веб-страница';
+$lang['web_server_custom_configuration'] = 'обичај конфигурација';
+$lang['web_server_custom_configuration_warning'] = 'Една стара или обичај конфигурациската датотека е откриен, но се уште може да ги конфигурирате поставките за пристап испратите користење на интернет формуларот подолу. Кликнете на следниов копчето за да започнете со користење на стандардна конфигурација, генерирани од апликацијата на веб серверот.';
+$lang['web_server_directory_alias'] = 'директориум Алијас';
+$lang['web_server_file_server_state_invalid'] = 'серверот за датотеки држава е валиден.';
+$lang['web_server_file_server_upload'] = 'Серверот за датотеки Додадени';
+$lang['web_server_ftp_state_invalid'] = 'FTP држава е валиден.';
+$lang['web_server_ftp_upload'] = 'FTP upload';
+$lang['web_server_group_invalid'] = 'Група во невалиден.';
+$lang['web_server_hostname_aliases'] = 'Домаќинот на Алијаси';
+$lang['web_server_live_site'] = 'во живо на сајтот';
+$lang['web_server_review_standard_configuration'] = 'Прегледајте стандардна конфигурација';
+$lang['web_server_server_name'] = 'име на серверот';
+$lang['web_server_server_name_invalid'] = 'Невалидно име на серверот.';
+$lang['web_server_site_invalid'] = 'Име на веб-сајт не е валиден.';
+$lang['web_server_test_site'] = 'тест на сајтот';
+$lang['web_server_upload_access'] = 'Внеси пристап';
+$lang['web_server_web_application'] = 'веб апликација';
+$lang['web_server_web_application_settings'] = 'Прилагодување на веб апликации';
+$lang['web_server_web_applications'] = 'веб апликации';
+$lang['web_server_web_server'] = 'веб Сервер';
+$lang['web_server_web_site'] = 'веб-страница';
+$lang['web_server_web_site_hostname'] = 'Хост Веб-сајт';
+$lang['web_server_web_sites'] = 'веб-страниците';
