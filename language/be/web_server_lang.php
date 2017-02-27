8ec78448bf8680ab919a8c12d4e4ccfb44b7e842
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:29:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'псеўданімы';
+$lang['web_server_aliases_invalid'] = 'Псеўданімы поля з&#39;яўляецца несапраўдным.';
+$lang['web_server_app_description'] = 'Прыкладанне Web-сервер можа быць выкарыстаны для стварэння простых аўтаномных вэб-сайтаў або як частка больш шырокай інфраструктуры для разгортвання вэб-прыкладанняў з выкарыстаннем тэхналогій, такіх як PHP, MySQL і JavaScript.';
+$lang['web_server_app_name'] = 'вэб-сервер';
+$lang['web_server_configure_default_web_site'] = 'Налада Default Web Site';
+$lang['web_server_custom_configuration'] = 'прыстасаваныя наладкі';
+$lang['web_server_custom_configuration_warning'] = 'Стары або карыстацкі канфігурацыйны файл быў знойдзены, але вы ўсё яшчэ можаце наладзіць параметры доступу для загрузкі з дапамогай вэб-форму ніжэй. Націсніце на наступную кнопку, каб пачаць выкарыстоўваць стандартную канфігурацыю, генераваны дадаткам вэб-сервера.';
+$lang['web_server_directory_alias'] = 'даведнік Аліяс';
+$lang['web_server_file_server_state_invalid'] = 'стан файлавага сервера ня дзейнічае.';
+$lang['web_server_file_server_upload'] = 'Файлавы сервер Загрузіць';
+$lang['web_server_ftp_state_invalid'] = 'FTP стан з&#39;яўляецца несапраўдным.';
+$lang['web_server_ftp_upload'] = 'FTP Загрузіць';
+$lang['web_server_group_invalid'] = 'Група ў несапраўдныя.';
+$lang['web_server_hostname_aliases'] = 'псеўданімы Hostname';
+$lang['web_server_live_site'] = 'жывы сайт';
+$lang['web_server_review_standard_configuration'] = 'Агляд стандартнай канфігурацыі';
+$lang['web_server_server_name'] = 'Імя сервера';
+$lang['web_server_server_name_invalid'] = 'Няправільнае імя сервера.';
+$lang['web_server_site_invalid'] = 'Імя вэб-сайта з&#39;яўляецца несапраўдным.';
+$lang['web_server_test_site'] = 'месца выпрабаванняў';
+$lang['web_server_upload_access'] = 'загрузіць Access';
+$lang['web_server_web_application'] = 'вэб-дадатак';
+$lang['web_server_web_application_settings'] = 'Налады вэб-прыкладанняў';
+$lang['web_server_web_applications'] = 'вэб-прыкладанні';
+$lang['web_server_web_server'] = 'вэб-сервер';
+$lang['web_server_web_site'] = 'вэб-сайт';
+$lang['web_server_web_site_hostname'] = 'Hostname Вэб-сайт';
+$lang['web_server_web_sites'] = 'вэб-сайты';
