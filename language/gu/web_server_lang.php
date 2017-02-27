d2894dbc139b4ad6e3d29e04d81ffec50a50fc49
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:32:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'ઉપનામ';
+$lang['web_server_aliases_invalid'] = 'ઉપનામ ક્ષેત્ર અમાન્ય છે.';
+$lang['web_server_app_description'] = 'વેબ સર્વર એપ્લિકેશન અથવા સરળ એકલ વેબ સાઇટ્સ બનાવવા માટે વાપરી શકાય છે કે વ્યાપક ઈન્ફ્રાસ્ટ્રક્ચર PHP, MySQL, અને જાવાસ્ક્રિપ્ટ જેવી ટેકનોલોજીનો ઉપયોગ વેબ આધારિત કાર્યક્રમો જમાવવા માટે ભાગ તરીકે.';
+$lang['web_server_app_name'] = 'વેબ સર્વર';
+$lang['web_server_configure_default_web_site'] = 'રૂપરેખાંકિત મૂળભૂત વેબ સાઇટ';
+$lang['web_server_custom_configuration'] = 'વૈવિધ્યપૂર્ણ રૂપરેખાંકન';
+$lang['web_server_custom_configuration_warning'] = 'જૂના અથવા વૈવિધ્યપૂર્ણ રૂપરેખાંકન ફાઈલ મળી આવી હતી, પરંતુ તમે હજુ પણ નીચે વેબ ફોર્મ નો ઉપયોગ કરીને અપલોડ ઍક્સેસ ને રૂપરેખાંકિત કરી શકો છો. નીચેના બટન પર ક્લિક કરો વેબ સર્વર એપ્લિકેશન દ્વારા પેદા પ્રમાણભૂત રૂપરેખાંકન ઉપયોગ શરૂ કરવા માટે.';
+$lang['web_server_directory_alias'] = 'ડિરેક્ટરી ઉપનામ';
+$lang['web_server_file_server_state_invalid'] = 'ફાઈલ સર્વર રાજ્ય અમાન્ય છે.';
+$lang['web_server_file_server_upload'] = 'ફાઈલ સર્વર અપલોડ';
+$lang['web_server_ftp_state_invalid'] = 'FTP રાજ્ય અમાન્ય છે.';
+$lang['web_server_ftp_upload'] = 'FTP અપલોડ કરો';
+$lang['web_server_group_invalid'] = 'અમાન્ય ગ્રુપ.';
+$lang['web_server_hostname_aliases'] = 'યજમાનનામ ઉપનામ';
+$lang['web_server_live_site'] = 'લાઈવ સાઇટ';
+$lang['web_server_review_standard_configuration'] = 'સમીક્ષા પ્રમાણભૂત રૂપરેખાંકન';
+$lang['web_server_server_name'] = 'સર્વર નામ';
+$lang['web_server_server_name_invalid'] = 'અમાન્ય સર્વર નામ.';
+$lang['web_server_site_invalid'] = 'વેબ સાઇટ નામ અમાન્ય છે.';
+$lang['web_server_test_site'] = 'ટેસ્ટ સાઇટ';
+$lang['web_server_upload_access'] = 'અપલોડ ઍક્સેસ';
+$lang['web_server_web_application'] = 'વેબ એપ્લિકેશન';
+$lang['web_server_web_application_settings'] = 'વેબ એપ્લિકેશન સેટિંગ્સ';
+$lang['web_server_web_applications'] = 'વેબ એપ્લિકેશન્સ';
+$lang['web_server_web_server'] = 'વેબ સર્વર';
+$lang['web_server_web_site'] = 'વેબસાઇટ';
+$lang['web_server_web_site_hostname'] = 'વેબ સાઇટ યજમાનનામ';
+$lang['web_server_web_sites'] = 'વેબ સાઇટ્સ';
