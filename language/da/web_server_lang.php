6222a22ff2b8f6c72b4f2e480aed374a46ec1867
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:31:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'aliasser';
+$lang['web_server_aliases_invalid'] = 'Aliaser felt er ugyldigt.';
+$lang['web_server_app_description'] = 'Web Server app kan bruges til at skabe simple standalone hjemmesider eller som del af en bredere infrastruktur til at implementere web-baserede applikationer ved hjælp af teknologier som PHP, MySQL, og JavaScript.';
+$lang['web_server_app_name'] = 'Web Server';
+$lang['web_server_configure_default_web_site'] = 'Konfigurer Default Web Site';
+$lang['web_server_custom_configuration'] = 'Tilpasset konfiguration';
+$lang['web_server_custom_configuration_warning'] = 'En gammel eller brugerdefineret konfigurationsfil blev fundet, men du kan stadig konfigurere upload adgang ved hjælp af web-formularen nedenfor. Klik på følgende knap for at begynde at bruge standard konfiguration, der genereres af webserver app.';
+$lang['web_server_directory_alias'] = 'Register Alias';
+$lang['web_server_file_server_state_invalid'] = 'File server tilstand er ugyldig.';
+$lang['web_server_file_server_upload'] = 'File Server Upload';
+$lang['web_server_ftp_state_invalid'] = 'FTP tilstand er ugyldig.';
+$lang['web_server_ftp_upload'] = 'FTP Upload';
+$lang['web_server_group_invalid'] = 'Gruppe i ugyldig.';
+$lang['web_server_hostname_aliases'] = 'hostname Aliases';
+$lang['web_server_live_site'] = 'live-site';
+$lang['web_server_review_standard_configuration'] = 'Gennemgå Standard Configuration';
+$lang['web_server_server_name'] = 'Servernavn';
+$lang['web_server_server_name_invalid'] = 'Ugyldigt servernavn.';
+$lang['web_server_site_invalid'] = 'Websted er ugyldigt.';
+$lang['web_server_test_site'] = 'Test site';
+$lang['web_server_upload_access'] = 'Upload Access';
+$lang['web_server_web_application'] = 'Web Application';
+$lang['web_server_web_application_settings'] = 'Web Application Settings';
+$lang['web_server_web_applications'] = 'Web Applications';
+$lang['web_server_web_server'] = 'Web Server';
+$lang['web_server_web_site'] = 'Internet side';
+$lang['web_server_web_site_hostname'] = 'Websted Hostname';
+$lang['web_server_web_sites'] = 'websteder';
