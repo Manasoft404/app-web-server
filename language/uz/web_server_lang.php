24f20955d50bc863c56d2b131a7b88abd0fb7751
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:39:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'laqab';
+$lang['web_server_aliases_invalid'] = 'Laqab dala haqiqiy emas.';
+$lang['web_server_app_description'] = 'Web Server ilova oddiy standalone veb-saytlar yaratish uchun ishlatiladi yoki mumkin PHP, MySQL, va JavaScript kabi texnologiyalar yordamida veb-asoslangan dasturini o&#39;rnatish uchun keng infratuzilmasini bir qismi sifatida.';
+$lang['web_server_app_name'] = 'Web Server';
+$lang['web_server_configure_default_web_site'] = 'Default veb-sayt moslashName';
+$lang['web_server_custom_configuration'] = 'Custom Configuration';
+$lang['web_server_custom_configuration_warning'] = 'Eski yoki maxsus konfiguratsiya fayli aniqlandi, lekin siz hali quyidagi veb shakl orqali upload kirish moslamalarini moslashingiz mumkin. Web Server ilova tomonidan hosil standart konfiguratsiyani foydalanishni boshlash uchun quyidagi tugmani bosing.';
+$lang['web_server_directory_alias'] = 'Directory Alias';
+$lang['web_server_file_server_state_invalid'] = 'File server davlat haqiqiy emas.';
+$lang['web_server_file_server_upload'] = 'Server Fayl yuklash';
+$lang['web_server_ftp_state_invalid'] = 'FTP davlat haqiqiy emas.';
+$lang['web_server_ftp_upload'] = 'FTP upload';
+$lang['web_server_group_invalid'] = 'haqiqiy emas guruh.';
+$lang['web_server_hostname_aliases'] = 'hostname laqab';
+$lang['web_server_live_site'] = 'jonli Sayt';
+$lang['web_server_review_standard_configuration'] = 'Standard Konfiguratsiya ko&#39;rib';
+$lang['web_server_server_name'] = 'Server nomi';
+$lang['web_server_server_name_invalid'] = 'Noto&#39;g&#39;ri server nomi.';
+$lang['web_server_site_invalid'] = 'Veb-sayt nomi yaroqsiz.';
+$lang['web_server_test_site'] = 'Test Sayt';
+$lang['web_server_upload_access'] = 'Access yuklash';
+$lang['web_server_web_application'] = 'Web Application';
+$lang['web_server_web_application_settings'] = 'Web Application Settings';
+$lang['web_server_web_applications'] = 'Web Ilovalar';
+$lang['web_server_web_server'] = 'Web Server';
+$lang['web_server_web_site'] = 'veb-sayt';
+$lang['web_server_web_site_hostname'] = 'Veb-sayt Hostname';
+$lang['web_server_web_sites'] = 'veb-saytlar';
