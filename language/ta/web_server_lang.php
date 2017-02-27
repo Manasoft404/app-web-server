159f851637a084ce701f4795cfa6fbe373472c23
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:38:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'மாற்றுப்பெயர்கள்';
+$lang['web_server_aliases_invalid'] = 'மாற்றுப்பெயர்கள் புலம் செல்லாதது.';
+$lang['web_server_app_description'] = 'வலை சர்வர் பயன்பாட்டை எளிய முழுமையான இணைய தளங்களை உருவாக்க பயன்படுத்தப்படும் அல்லது முடியும் PHP, MySQL, மற்றும் JavaScript போன்ற தொழில்நுட்பங்களை பயன்படுத்தி வலை அடிப்படையிலான பயன்பாடுகள் வரிசைப்படுத்த ஒரு பரந்த உள்கட்டமைப்பு பகுதியாக.';
+$lang['web_server_app_name'] = 'வலை சர்வர்';
+$lang['web_server_configure_default_web_site'] = 'இயல்புநிலை வலை தளத்தில் கட்டமைக்க';
+$lang['web_server_custom_configuration'] = 'விருப்ப கட்டமைப்பு';
+$lang['web_server_custom_configuration_warning'] = 'ஒரு பழைய அல்லது விருப்ப கட்டமைப்பு கோப்பு கண்டறியப்பட்டது, ஆனால் நீங்கள் இன்னும் கீழே இணைய படிவத்தை பயன்படுத்தி பதிவேற்ற அணுகல் அமைப்புகளை கட்டமைக்க முடியும். வலை சர்வர் பயன்பாட்டால் உருவாக்கப்பட்ட தரமான கட்டமைப்பு பயன்படுத்தி தொடங்க பின்வரும் பொத்தானைக் கிளிக் செய்யவும்.';
+$lang['web_server_directory_alias'] = 'ஏற்கனவே அலைஸ்';
+$lang['web_server_file_server_state_invalid'] = 'கோப்பு சர்வர் மாநில தவறானது.';
+$lang['web_server_file_server_upload'] = 'சர்வர் கோப்பு பதிவேற்ற';
+$lang['web_server_ftp_state_invalid'] = 'FTP, மாநில தவறானது.';
+$lang['web_server_ftp_upload'] = 'பதிவேற்ற FTP';
+$lang['web_server_group_invalid'] = 'தவறான குழுமத்தின்.';
+$lang['web_server_hostname_aliases'] = 'புரவலன் பெயர் மாற்றுப்பெயர்கள்';
+$lang['web_server_live_site'] = 'நேரடி தள';
+$lang['web_server_review_standard_configuration'] = 'தரமான கட்டமைப்பு ஆய்வு';
+$lang['web_server_server_name'] = 'சர்வர் பெயர்';
+$lang['web_server_server_name_invalid'] = 'தவறான சர்வர் பெயர்.';
+$lang['web_server_site_invalid'] = 'வலை தளத்தில் பெயர் தவறானது.';
+$lang['web_server_test_site'] = 'டெஸ்ட் தள';
+$lang['web_server_upload_access'] = 'அணுகல் பதிவேற்ற';
+$lang['web_server_web_application'] = 'வலை பயன்பாடு';
+$lang['web_server_web_application_settings'] = 'வெப் அப்ளிகேஷன் அமைப்புகள்';
+$lang['web_server_web_applications'] = 'வலை பயன்பாடுகள்';
+$lang['web_server_web_server'] = 'வலை சர்வர்';
+$lang['web_server_web_site'] = 'வலைத்தளம்';
+$lang['web_server_web_site_hostname'] = 'வலை தள புரவலன்';
+$lang['web_server_web_sites'] = 'இணைய தளங்கள்';
