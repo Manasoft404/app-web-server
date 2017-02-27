2ed3d24e05a2e037a6cf5b8692f6840dfb81e657
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:39:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'عرفیتیں';
+$lang['web_server_aliases_invalid'] = 'عرفیتیں فیلڈ غلط ہے.';
+$lang['web_server_app_description'] = 'ویب سرور ایپ یا سادہ اسٹینڈ ویب سائٹس کو پیدا کرنے کے استعمال کیا جا سکتا ہے پی ایچ پی، ایس کیو ایل، اور جاوا سکرپٹ کی طرح ٹیکنالوجی کا استعمال کرتے ہوئے ویب کی بنیاد پر ایپلی کیشنز کو تعینات کرنے کے ایک وسیع تر بنیادی ڈھانچے کا حصہ کے طور پر.';
+$lang['web_server_app_name'] = 'ویب سرور';
+$lang['web_server_configure_default_web_site'] = 'پہلے سے طے شدہ ویب سائٹ کو ترتیب';
+$lang['web_server_custom_configuration'] = 'اپنی مرضی کی ترتیب';
+$lang['web_server_custom_configuration_warning'] = 'ایک پرانا یا اپنی مرضی کے مطابق ترتیب دینے کی فائل کا پتہ چلا تھا، لیکن آپ کو اب ذیل میں ویب فارم استعمال کو اپ لوڈ کریں رسائی کی ترتیبات کو ترتیب دے سکتے. ویب سرور کو اے پی پی کی طرف سے پیدا معیاری ترتیب کا استعمال کرتے ہوئے شروع کرنے کے لئے درج ذیل بٹن پر کلک کریں.';
+$lang['web_server_directory_alias'] = 'ڈائریکٹری عرفی';
+$lang['web_server_file_server_state_invalid'] = 'فائل سرور ریاست باطل ہے.';
+$lang['web_server_file_server_upload'] = 'فائل سرور اپ لوڈ کریں';
+$lang['web_server_ftp_state_invalid'] = 'FTP ریاست باطل ہے.';
+$lang['web_server_ftp_upload'] = 'FTP اپ لوڈ کریں';
+$lang['web_server_group_invalid'] = 'باطل میں گروپ.';
+$lang['web_server_hostname_aliases'] = 'میزبان نام عرفی نام';
+$lang['web_server_live_site'] = 'لائیو ویب سائٹ';
+$lang['web_server_review_standard_configuration'] = 'معیاری ترتیب کا جائزہ لیں';
+$lang['web_server_server_name'] = 'خدمت گار کا نام';
+$lang['web_server_server_name_invalid'] = 'غلط سرور کا نام.';
+$lang['web_server_site_invalid'] = 'ویب سائٹ کا نام غلط ہے.';
+$lang['web_server_test_site'] = 'ٹیسٹ سائٹ';
+$lang['web_server_upload_access'] = 'رسائی میں اپ لوڈ کریں';
+$lang['web_server_web_application'] = 'ویب ایپلیکیشن';
+$lang['web_server_web_application_settings'] = 'ویب ایپلیکیشن کی ترتیبات کے';
+$lang['web_server_web_applications'] = 'ویب اطلاقات';
+$lang['web_server_web_server'] = 'ویب سرور';
+$lang['web_server_web_site'] = 'ویب سائٹ';
+$lang['web_server_web_site_hostname'] = 'ویب سائٹ کا میزبان نام';
+$lang['web_server_web_sites'] = 'ویب سائٹس';
