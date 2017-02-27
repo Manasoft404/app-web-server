84c543b61ac13a7efc5389eee092816311e40927
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:29:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'emërtimet';
+$lang['web_server_aliases_invalid'] = 'fushë emërtimet është e pavlefshme.';
+$lang['web_server_app_description'] = 'App Web Server mund të përdoret për të krijuar web faqet thjeshta të pavarur ose si pjesë e një infrastrukture më të gjerë për të vendosur web-bazuar aplikacionet duke përdorur teknologji të tilla si PHP, MySQL, dhe JavaScript.';
+$lang['web_server_app_name'] = 'web Server';
+$lang['web_server_configure_default_web_site'] = 'Konfiguro Default Web Site';
+$lang['web_server_custom_configuration'] = 'Custom Konfigurimi';
+$lang['web_server_custom_configuration_warning'] = 'Një fotografi e vjetër apo porosi konfigurimit është zbuluar, por ju ende mund të konfiguroni cilësimet e aksesit upload duke përdorur formularin e internetit më poshtë. Kliko në butonin e mëposhtëm për të filluar duke përdorur konfigurimin standarde gjeneruar nga app Web Server.';
+$lang['web_server_directory_alias'] = 'Lista Alias';
+$lang['web_server_file_server_state_invalid'] = 'Shteti server File është i pavlefshëm.';
+$lang['web_server_file_server_upload'] = 'Paraqesë Server Ngarko';
+$lang['web_server_ftp_state_invalid'] = 'Shteti FTP është e pavlefshme.';
+$lang['web_server_ftp_upload'] = 'FTP Ngarko';
+$lang['web_server_group_invalid'] = 'Grupi në të pavlefshme.';
+$lang['web_server_hostname_aliases'] = 'hostname Aliases';
+$lang['web_server_live_site'] = 'jetojnë Faqes';
+$lang['web_server_review_standard_configuration'] = 'Rishikoni Standard Konfigurimi';
+$lang['web_server_server_name'] = 'Emri i serverit';
+$lang['web_server_server_name_invalid'] = 'Emri i pavlefshëm server.';
+$lang['web_server_site_invalid'] = 'Emri Web site është i pavlefshëm.';
+$lang['web_server_test_site'] = 'Test Site';
+$lang['web_server_upload_access'] = 'Ngarko Qasja';
+$lang['web_server_web_application'] = 'web Application';
+$lang['web_server_web_application_settings'] = 'Cilësimet Web aplikimit';
+$lang['web_server_web_applications'] = 'Web Aplikimet';
+$lang['web_server_web_server'] = 'web Server';
+$lang['web_server_web_site'] = 'web Site';
+$lang['web_server_web_site_hostname'] = 'Hostname Web Site';
+$lang['web_server_web_sites'] = 'Web Faqe';
