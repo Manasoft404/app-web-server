d5e9ec656b2dcd38f96b6766dd17d20485648343
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:31:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'aliakset';
+$lang['web_server_aliases_invalid'] = 'Aliases ei kelpaa.';
+$lang['web_server_app_description'] = 'Web-palvelin-sovellus voidaan luoda yksinkertaisia ​​standalone verkkosivujen tai osana laajempaa infrastruktuurin käyttöön web-pohjaisia ​​sovelluksia käyttäen tekniikoita, kuten PHP, MySQL ja JavaScript.';
+$lang['web_server_app_name'] = 'Verkkopalvelin';
+$lang['web_server_configure_default_web_site'] = 'Määritä Default Web Site';
+$lang['web_server_custom_configuration'] = 'Custom Configuration';
+$lang['web_server_custom_configuration_warning'] = 'Vanha tai mukautettu asetustiedostoa havaittiin, mutta voit silti määrittää upload käyttöoikeusasetuksia käyttämällä web lomakkeella. Klikkaa seuraavaa painiketta alkaa käyttää vakiokokoonpanoa tuottamat Web Server sovelluksen.';
+$lang['web_server_directory_alias'] = 'Directory Alias';
+$lang['web_server_file_server_state_invalid'] = 'Tiedostopalvelin tila ei kelpaa.';
+$lang['web_server_file_server_upload'] = 'File Server Upload';
+$lang['web_server_ftp_state_invalid'] = 'FTP tila ei kelpaa.';
+$lang['web_server_ftp_upload'] = 'FTP Upload';
+$lang['web_server_group_invalid'] = 'Group kelpaa.';
+$lang['web_server_hostname_aliases'] = 'hostname Aliakset';
+$lang['web_server_live_site'] = 'Elävät Site';
+$lang['web_server_review_standard_configuration'] = 'Review Vakiokokoonpanon';
+$lang['web_server_server_name'] = 'Palvelimen nimi';
+$lang['web_server_server_name_invalid'] = 'Virheellinen palvelimen nimi.';
+$lang['web_server_site_invalid'] = 'Web-sivuston nimi on virheellinen.';
+$lang['web_server_test_site'] = 'Test Site';
+$lang['web_server_upload_access'] = 'Lataa Access';
+$lang['web_server_web_application'] = 'Web Application';
+$lang['web_server_web_application_settings'] = 'Web Application Settings';
+$lang['web_server_web_applications'] = 'Web-sovellukset';
+$lang['web_server_web_server'] = 'Verkkopalvelin';
+$lang['web_server_web_site'] = 'Verkkosivusto';
+$lang['web_server_web_site_hostname'] = 'Sivuston Hostname';
+$lang['web_server_web_sites'] = 'Web-sivustot';
