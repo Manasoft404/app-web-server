7715df6ad1ed5fe3d1ee340ecc39cb00952ebed1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:30:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'ওরফে';
+$lang['web_server_aliases_invalid'] = 'ওরফে ক্ষেত্র অবৈধ.';
+$lang['web_server_app_description'] = 'ওয়েব সার্ভার অ্যাপ্লিকেশন বা সহজ স্বতন্ত্র ওয়েব সাইট তৈরি করার জন্য ব্যবহার করা যেতে পারে একটি বৃহত্তর অবকাঠামো পিএইচপি, মাইএসকিউএল, এবং জাভাস্ক্রিপ্ট মত প্রযুক্তি ব্যবহার করে ওয়েব ভিত্তিক অ্যাপ্লিকেশন মোতায়েনের অংশ হিসেবে.';
+$lang['web_server_app_name'] = 'ওয়েব সার্ভার';
+$lang['web_server_configure_default_web_site'] = 'কনফিগার করুন ডিফল্ট ওয়েব সাইট';
+$lang['web_server_custom_configuration'] = 'কাস্টম কনফিগারেশন';
+$lang['web_server_custom_configuration_warning'] = 'একটি পুরানো বা কাস্টম কনফিগারেশন ফাইল সনাক্ত করা হয়েছে, কিন্তু আপনি এখনও নিচে ওয়েব ফর্ম ব্যবহার আপলোড অ্যাক্সেস সেটিংস কনফিগার করতে পারেন. নিম্নলিখিত বাটনে ক্লিক করুন ওয়েব সার্ভার অ্যাপ্লিকেশন দ্বারা উত্পন্ন মান কনফিগারেশন ব্যবহার শুরু করতে.';
+$lang['web_server_directory_alias'] = 'নির্দেশিকা ওরফে';
+$lang['web_server_file_server_state_invalid'] = 'ফাইল সার্ভার রাষ্ট্র অবৈধ.';
+$lang['web_server_file_server_upload'] = 'ফাইল সার্ভার আপলোড';
+$lang['web_server_ftp_state_invalid'] = 'FTP- র রাষ্ট্র অবৈধ.';
+$lang['web_server_ftp_upload'] = 'FTP আপলোড';
+$lang['web_server_group_invalid'] = 'অকার্যকর গ্রুপ.';
+$lang['web_server_hostname_aliases'] = 'হোস্টনাম ওরফে';
+$lang['web_server_live_site'] = 'লাইভ সাইট';
+$lang['web_server_review_standard_configuration'] = 'পর্যালোচনা স্ট্যান্ডার্ড কনফিগারেশন';
+$lang['web_server_server_name'] = 'সার্ভার নাম';
+$lang['web_server_server_name_invalid'] = 'অবৈধ সার্ভার নাম.';
+$lang['web_server_site_invalid'] = 'ওয়েব সাইটের নাম অবৈধ.';
+$lang['web_server_test_site'] = 'টেস্ট সাইট';
+$lang['web_server_upload_access'] = 'আপলোড অ্যাক্সেস';
+$lang['web_server_web_application'] = 'ওয়েব অ্যাপ্লিকেশন';
+$lang['web_server_web_application_settings'] = 'ওয়েব অ্যাপ্লিকেশন সেটিংস';
+$lang['web_server_web_applications'] = 'ওয়েব অ্যাপ্লিকেশন';
+$lang['web_server_web_server'] = 'ওয়েব সার্ভার';
+$lang['web_server_web_site'] = 'ওয়েব সাইট';
+$lang['web_server_web_site_hostname'] = 'ওয়েব সাইট হোস্ট';
+$lang['web_server_web_sites'] = 'ওয়েব সাইট';
