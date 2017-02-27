0ac10f2ab27a56ae63d445cd8167cbde4fb2e484
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:35:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'ഇരട്ടപ്പേരുകൾ';
+$lang['web_server_aliases_invalid'] = 'ഇരട്ടപ്പേരുകൾ ഫീൽഡ് അസാധുവാണ്.';
+$lang['web_server_app_description'] = 'വെബ് സെർവർ അപ്ലിക്കേഷൻ ലളിതമായ സ്റ്റാൻഡ്എലോൺ വെബ് സൈറ്റുകൾ സൃഷ്ടിക്കാൻ ഉപയോഗിച്ച അല്ലെങ്കിൽ പി.എച്ച്.പി, മൈ, JavaScript പോലുള്ള സാങ്കേതികവിദ്യകൾ ഉപയോഗിച്ച് വെബ്-അധിഷ്ടിത പ്രയോഗങ്ങളുടെ വിന്യസിക്കാൻ വിശാലമായ ഇൻഫ്രാസ്ട്രക്ചർ ഭാഗമായാണ് കഴിയും.';
+$lang['web_server_app_name'] = 'വെബ് സെർവർ';
+$lang['web_server_configure_default_web_site'] = 'സഹജമായ വെബ് സൈറ്റ് ക്രമീകരിക്കുക';
+$lang['web_server_custom_configuration'] = 'കസ്റ്റം ക്രമീകരണം';
+$lang['web_server_custom_configuration_warning'] = 'പഴയ ഒരു അല്ലെങ്കിൽ കസ്റ്റം ക്രമീകരണ ഫയൽ കണ്ടെത്താനായില്ല, എന്നാലും ചുവടെ തുടർന്നും വെബ് ഫോം ഉപയോഗിച്ച് അപ്ലോഡ് ആക്സസ് ക്രമികരിക്കുവാൻ സാധിക്കുന്നു. വെബ് സെർവർ അപ്ലിക്കേഷൻ സൃഷ്ടിച്ച സാധാരണ കോൺഫിഗറേഷൻ ഉപയോഗിച്ച് തുടങ്ങാൻ ഇനിപ്പറയുന്ന ബട്ടണിൽ ക്ലിക്ക് ചെയ്യുക.';
+$lang['web_server_directory_alias'] = 'ഡയറക്ടറി അലിയാസ്';
+$lang['web_server_file_server_state_invalid'] = 'ഫയൽ സെർവർ സംസ്ഥാന അസാധുവാണ്.';
+$lang['web_server_file_server_upload'] = 'ഫയൽ സെർവർ അപ്ലോഡ്';
+$lang['web_server_ftp_state_invalid'] = 'എഫ്ടിപി സംസ്ഥാന അസാധുവാണ്.';
+$lang['web_server_ftp_upload'] = 'എഫ്ടിപി അപ്ലോഡ്';
+$lang['web_server_group_invalid'] = 'അസാധുവാണ് ഗ്രൂപ്പ്.';
+$lang['web_server_hostname_aliases'] = 'ഹോസ്റ്റ്നാമം ഇരട്ടപ്പേരുകൾ';
+$lang['web_server_live_site'] = 'ലൈവ് സൈറ്റ്';
+$lang['web_server_review_standard_configuration'] = 'റിവ്യൂ സ്റ്റാൻഡേർഡ് ക്രമീകരണം';
+$lang['web_server_server_name'] = 'സെർവർ പേര്';
+$lang['web_server_server_name_invalid'] = 'അസാധുവായ സെർവർ പേര്.';
+$lang['web_server_site_invalid'] = 'വെബ് സൈറ്റ് പേര് അസാധുവാണ്.';
+$lang['web_server_test_site'] = 'ടെസ്റ്റ് സൈറ്റ്';
+$lang['web_server_upload_access'] = 'പ്രവേശനം അപ്ലോഡ്';
+$lang['web_server_web_application'] = 'വെബ് ആപ്ലിക്കേഷന്';
+$lang['web_server_web_application_settings'] = 'വെബ് ആപ്ലിക്കേഷൻ ക്രമീകരണങ്ങൾ';
+$lang['web_server_web_applications'] = 'വെബ് അപ്ലിക്കേഷനുകൾ';
+$lang['web_server_web_server'] = 'വെബ് സെർവർ';
+$lang['web_server_web_site'] = 'വെബ് സൈറ്റ്';
+$lang['web_server_web_site_hostname'] = 'വെബ് സൈറ്റ് ഹോസ്റ്റ്നാമം';
+$lang['web_server_web_sites'] = 'വെബ് സൈറ്റുകൾ';
