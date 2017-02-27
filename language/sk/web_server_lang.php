bac893eaab94cfca0a5a153891d71b6e1965a538
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:37:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'prezývky';
+$lang['web_server_aliases_invalid'] = 'Prezývky poľa je neplatné.';
+$lang['web_server_app_description'] = 'Aplikácia Web Server možno použiť na vytvorenie jednoduchej samostatnej webovej stránky, alebo ako súčasť širšieho infraštruktúry pre nasadenie webových aplikácií pomocou technológií, ako je PHP, MySQL a JavaScript.';
+$lang['web_server_app_name'] = 'webový server';
+$lang['web_server_configure_default_web_site'] = 'Konfigurácia predvolený web';
+$lang['web_server_custom_configuration'] = 'Custom Configuration';
+$lang['web_server_custom_configuration_warning'] = 'Starý alebo vlastný konfiguračný súbor bol zistený, ale stále môžete konfigurovať nastavenia prístupu nahrať pomocou webového formulára nižšie. Kliknúť na nasledujúci odkaz začať používať štandardnú konfiguráciu vytváranej aplikácie webového servera.';
+$lang['web_server_directory_alias'] = 'Directory Alias';
+$lang['web_server_file_server_state_invalid'] = 'Stavový server súbor je neplatný.';
+$lang['web_server_file_server_upload'] = 'File Server nahrávanie';
+$lang['web_server_ftp_state_invalid'] = 'FTP stav je neplatný.';
+$lang['web_server_ftp_upload'] = 'FTP uploadu';
+$lang['web_server_group_invalid'] = 'Skupina v neplatný.';
+$lang['web_server_hostname_aliases'] = 'hostname aliasov';
+$lang['web_server_live_site'] = 'živé Site';
+$lang['web_server_review_standard_configuration'] = 'Skontrolujte štandardné konfigurácií';
+$lang['web_server_server_name'] = 'name Server';
+$lang['web_server_server_name_invalid'] = 'Neplatný názov servera.';
+$lang['web_server_site_invalid'] = 'Názov webu je neplatná.';
+$lang['web_server_test_site'] = 'test Site';
+$lang['web_server_upload_access'] = 'nahrávanie Access';
+$lang['web_server_web_application'] = 'web Application';
+$lang['web_server_web_application_settings'] = 'Nastavenie webovej aplikácie';
+$lang['web_server_web_applications'] = 'webové aplikácie';
+$lang['web_server_web_server'] = 'webový server';
+$lang['web_server_web_site'] = 'web Site';
+$lang['web_server_web_site_hostname'] = 'Názov hostiteľa Web Site';
+$lang['web_server_web_sites'] = 'webové stránky';
