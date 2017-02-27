f10ee4cc932fe71a33f47dcb1df6386bff470a59
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:36:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'aliassen';
+$lang['web_server_aliases_invalid'] = 'Alias ​​veld is ongeldig.';
+$lang['web_server_app_description'] = 'De Web Server app kan worden gebruikt om eenvoudige stand-alone websites te maken of als onderdeel van een bredere infrastructuur om web-based applicaties met behulp van technologieën zoals PHP, MySQL, en JavaScript in te zetten.';
+$lang['web_server_app_name'] = 'Web Server';
+$lang['web_server_configure_default_web_site'] = 'Configureren Default Web Site';
+$lang['web_server_custom_configuration'] = 'aangepaste configuratie';
+$lang['web_server_custom_configuration_warning'] = 'Een oude of aangepaste configuratie bestand werd gedetecteerd, maar u kunt nog steeds de toegang uploadinstellingen via het web onderstaand formulier in te configureren. Klik op de onderstaande knop om te beginnen met behulp van de standaard configuratie gegenereerd door de Web Server app.';
+$lang['web_server_directory_alias'] = 'directory Alias';
+$lang['web_server_file_server_state_invalid'] = 'Bestandsserver staat is ongeldig.';
+$lang['web_server_file_server_upload'] = 'File Server Upload';
+$lang['web_server_ftp_state_invalid'] = 'FTP staat is ongeldig.';
+$lang['web_server_ftp_upload'] = 'FTP uploaden';
+$lang['web_server_group_invalid'] = 'Group in ongeldig.';
+$lang['web_server_hostname_aliases'] = 'hostname Alias';
+$lang['web_server_live_site'] = 'live-Site';
+$lang['web_server_review_standard_configuration'] = 'Herzien Standard Configuration';
+$lang['web_server_server_name'] = 'Server naam';
+$lang['web_server_server_name_invalid'] = 'Ongeldige servernaam.';
+$lang['web_server_site_invalid'] = 'Website naam is ongeldig.';
+$lang['web_server_test_site'] = 'Testpagina';
+$lang['web_server_upload_access'] = 'Upload Access';
+$lang['web_server_web_application'] = 'Web applicatie';
+$lang['web_server_web_application_settings'] = 'Web Application Settings';
+$lang['web_server_web_applications'] = 'web Applications';
+$lang['web_server_web_server'] = 'Web Server';
+$lang['web_server_web_site'] = 'Website';
+$lang['web_server_web_site_hostname'] = 'Website hostnaam';
+$lang['web_server_web_sites'] = 'web sites';
