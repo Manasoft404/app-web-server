3a7df6f907b6473e949711ea03b24a9481b5fe29
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:32:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'उपनाम';
+$lang['web_server_aliases_invalid'] = 'उपनाम क्षेत्र अमान्य है।';
+$lang['web_server_app_description'] = 'वेब सर्वर एप्लिकेशन या सरल स्टैंडअलोन वेब साइटों को बनाने के लिए इस्तेमाल किया जा सकता है एक व्यापक बुनियादी सुविधाओं के PHP, MySQL, और जावास्क्रिप्ट की तरह प्रौद्योगिकी का उपयोग कर वेब आधारित अनुप्रयोगों को तैनात करने के भाग के रूप में।';
+$lang['web_server_app_name'] = 'वेब सर्वर';
+$lang['web_server_configure_default_web_site'] = 'कॉन्फ़िगर डिफ़ॉल्ट वेब साइट';
+$lang['web_server_custom_configuration'] = 'कस्टम विन्यास';
+$lang['web_server_custom_configuration_warning'] = 'एक पुराने या कस्टम विन्यास फाइल का पता चला था, लेकिन आप अभी भी नीचे वेब प्रपत्र का उपयोग अपलोड पहुँच सेटिंग्स कॉन्फ़िगर कर सकते हैं। निम्न बटन पर क्लिक करें वेब सर्वर एप्लिकेशन द्वारा उत्पन्न मानक विन्यास का उपयोग शुरू करने के लिए।';
+$lang['web_server_directory_alias'] = 'निर्देशिका उर्फ';
+$lang['web_server_file_server_state_invalid'] = 'फ़ाइल सर्वर राज्य अमान्य है।';
+$lang['web_server_file_server_upload'] = 'फ़ाइल सर्वर अपलोड';
+$lang['web_server_ftp_state_invalid'] = 'एफ़टीपी राज्य अमान्य है।';
+$lang['web_server_ftp_upload'] = 'एफ़टीपी अपलोड';
+$lang['web_server_group_invalid'] = 'अवैध में समूह।';
+$lang['web_server_hostname_aliases'] = 'मेजबाननाम नामः';
+$lang['web_server_live_site'] = 'लाइव साइट';
+$lang['web_server_review_standard_configuration'] = 'समीक्षा मानक विन्यास';
+$lang['web_server_server_name'] = 'सर्वर का नाम';
+$lang['web_server_server_name_invalid'] = 'अमान्य सर्वर नाम।';
+$lang['web_server_site_invalid'] = 'वेब साइट का नाम अवैध है।';
+$lang['web_server_test_site'] = 'परीक्षण स्थल';
+$lang['web_server_upload_access'] = 'शब्द पहुँच';
+$lang['web_server_web_application'] = 'वेब एप्लीकेशन';
+$lang['web_server_web_application_settings'] = 'वेब अनुप्रयोग सेटिंग';
+$lang['web_server_web_applications'] = 'वेब अनुप्रयोग';
+$lang['web_server_web_server'] = 'वेब सर्वर';
+$lang['web_server_web_site'] = 'वेबसाइट';
+$lang['web_server_web_site_hostname'] = 'वेब साइट होस्ट का नाम';
+$lang['web_server_web_sites'] = 'वेब साइटें';
