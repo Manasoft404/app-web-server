5c9c2c25b6bbe752f90f20492877a0b5faeab61b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:38:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'alias';
+$lang['web_server_aliases_invalid'] = 'campo de alias no es válido.';
+$lang['web_server_app_description'] = 'La aplicación Web Server se puede utilizar para crear sitios web simples independientes o como parte de una infraestructura más amplia para implementar aplicaciones basadas en la Web utilizando tecnologías como PHP, MySQL y JavaScript.';
+$lang['web_server_app_name'] = 'Servidor web';
+$lang['web_server_configure_default_web_site'] = 'Configurar sitio Web predeterminado';
+$lang['web_server_custom_configuration'] = 'Configuración personalizada';
+$lang['web_server_custom_configuration_warning'] = 'Se ha detectado un archivo de configuración anterior o costumbre, pero todavía se puede configurar los parámetros de acceso de carga utilizando el formulario web a continuación. Haga clic en el siguiente botón para comenzar a utilizar la configuración estándar generada por la aplicación de servidor Web.';
+$lang['web_server_directory_alias'] = 'Alias ​​del directorio';
+$lang['web_server_file_server_state_invalid'] = 'Estado del servidor de archivos no es válido.';
+$lang['web_server_file_server_upload'] = 'Servidor de archivos Subir';
+$lang['web_server_ftp_state_invalid'] = 'Estado de FTP no es válido.';
+$lang['web_server_ftp_upload'] = 'Cargar FTP';
+$lang['web_server_group_invalid'] = 'Grupo de inválido.';
+$lang['web_server_hostname_aliases'] = 'Los alias de nombre de host';
+$lang['web_server_live_site'] = 'sitio vivo';
+$lang['web_server_review_standard_configuration'] = 'Revisar la configuración estándar';
+$lang['web_server_server_name'] = 'Nombre del servidor';
+$lang['web_server_server_name_invalid'] = 'nombre del servidor no es válido.';
+$lang['web_server_site_invalid'] = 'nombre del sitio web no es válido.';
+$lang['web_server_test_site'] = 'Sitio de prueba';
+$lang['web_server_upload_access'] = 'Sube Acceso';
+$lang['web_server_web_application'] = 'Aplicación web';
+$lang['web_server_web_application_settings'] = 'Configuración de la aplicación Web';
+$lang['web_server_web_applications'] = 'Aplicaciones web';
+$lang['web_server_web_server'] = 'Servidor web';
+$lang['web_server_web_site'] = 'sitio web';
+$lang['web_server_web_site_hostname'] = 'Nombre de host del sitio Web';
+$lang['web_server_web_sites'] = 'Sitios web';
