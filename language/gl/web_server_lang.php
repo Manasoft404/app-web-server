8b43fa39fe43eaf22440a3b76b21744c5adc0e2c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:31:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'alias';
+$lang['web_server_aliases_invalid'] = 'campo Alias ​​é válido.';
+$lang['web_server_app_description'] = 'A aplicación web Server pode ser usada para crear simple sitios web independentes ou como parte dunha infraestrutura máis ampla para implantar aplicacións baseadas na web utilizando tecnoloxías como PHP, MySQL e JavaScript.';
+$lang['web_server_app_name'] = 'servidor web';
+$lang['web_server_configure_default_web_site'] = 'Configurar sitio defecto';
+$lang['web_server_custom_configuration'] = 'configuración personalizada';
+$lang['web_server_custom_configuration_warning'] = 'Un ficheiro de configuración de idade ou costume foi detectado, pero aínda pode configurar os axustes de acceso de subida usando o formulario web a continuación. Preme o botón de embaixo para comezar a usar a configuración por defecto que xera o programa Web Server.';
+$lang['web_server_directory_alias'] = 'directorio Alias';
+$lang['web_server_file_server_state_invalid'] = 'estado do servidor de ficheiros non é válido.';
+$lang['web_server_file_server_upload'] = 'File Server Cargar';
+$lang['web_server_ftp_state_invalid'] = 'Estado FTP non é válido.';
+$lang['web_server_ftp_upload'] = 'FTP Cargar';
+$lang['web_server_group_invalid'] = 'Grupo de válido.';
+$lang['web_server_hostname_aliases'] = 'hostname Alias';
+$lang['web_server_live_site'] = 'sitio en directo';
+$lang['web_server_review_standard_configuration'] = 'Comentar Configuración por defecto';
+$lang['web_server_server_name'] = 'Name Server';
+$lang['web_server_server_name_invalid'] = 'Nome de servidor non válido.';
+$lang['web_server_site_invalid'] = 'nome do sitio non é válido.';
+$lang['web_server_test_site'] = 'lugar de proba';
+$lang['web_server_upload_access'] = 'subir Acceso';
+$lang['web_server_web_application'] = 'aplicación web';
+$lang['web_server_web_application_settings'] = 'Configuración da aplicación web';
+$lang['web_server_web_applications'] = 'aplicacións web';
+$lang['web_server_web_server'] = 'servidor web';
+$lang['web_server_web_site'] = 'sitio web';
+$lang['web_server_web_site_hostname'] = 'Nome do servidor Web Site';
+$lang['web_server_web_sites'] = 'sitios web';
