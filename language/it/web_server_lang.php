c301ad4bbc9d6cfe7ad2eba401a75bef638edbf0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:33:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/web_server_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['web_server_aliases'] = 'Alias';
+$lang['web_server_aliases_invalid'] = 'campo Alias ​​non è valido.';
+$lang['web_server_app_description'] = 'L&#39;applicazione Web Server può essere utilizzato per creare semplici siti Web standalone o come parte di un&#39;infrastruttura più ampia per distribuire le applicazioni web-based che utilizzano tecnologie come PHP, MySQL, e JavaScript.';
+$lang['web_server_app_name'] = 'Web server';
+$lang['web_server_configure_default_web_site'] = 'Configurazione Sito Web predefinito';
+$lang['web_server_custom_configuration'] = 'Configurazione personalizzata';
+$lang['web_server_custom_configuration_warning'] = 'Un vecchio file di configurazione o personalizzato è stato rilevato, ma è ancora possibile configurare le impostazioni di accesso caricati utilizzando il modulo web sottostante. Fare clic sul seguente pulsante per iniziare a utilizzare la configurazione standard generato dalla applicazione Web Server.';
+$lang['web_server_directory_alias'] = 'directory Alias';
+$lang['web_server_file_server_state_invalid'] = 'stato del server di file non è valido.';
+$lang['web_server_file_server_upload'] = 'File Server Upload';
+$lang['web_server_ftp_state_invalid'] = 'stato FTP non è valido.';
+$lang['web_server_ftp_upload'] = 'FTP Upload';
+$lang['web_server_group_invalid'] = 'Gruppo non valido.';
+$lang['web_server_hostname_aliases'] = 'hostname Alias';
+$lang['web_server_live_site'] = 'live del sito';
+$lang['web_server_review_standard_configuration'] = 'Verifica configurazione standard';
+$lang['web_server_server_name'] = 'Nome del server';
+$lang['web_server_server_name_invalid'] = 'nome del server non valido.';
+$lang['web_server_site_invalid'] = 'nome del sito Web non è valido.';
+$lang['web_server_test_site'] = 'test Site';
+$lang['web_server_upload_access'] = 'Carica di accesso';
+$lang['web_server_web_application'] = 'Applicazione web';
+$lang['web_server_web_application_settings'] = 'Impostazioni Web Application';
+$lang['web_server_web_applications'] = 'Applicazioni Web';
+$lang['web_server_web_server'] = 'Web server';
+$lang['web_server_web_site'] = 'Sito web';
+$lang['web_server_web_site_hostname'] = 'Nome host del sito Web';
+$lang['web_server_web_sites'] = 'Siti Web';
